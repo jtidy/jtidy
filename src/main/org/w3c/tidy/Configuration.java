e586d73df1f12be0815216df0e531625803293b7
fgiust <fgiust@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2004-02-08 20:39:48 +0000

    removed useless variable initializations. Javadocs added

--- src/main/org/w3c/tidy/Configuration.java
+++ src/main/org/w3c/tidy/Configuration.java
@@ -69,7 +69,7 @@ import java.util.StringTokenizer;
 public class Configuration implements java.io.Serializable
 {
 
-    /* character encodings */
+    /** character encodings. */
     public static final int RAW = 0;
     public static final int ASCII = 1;
     public static final int LATIN1 = 2;
@@ -77,68 +77,117 @@ public class Configuration implements java.io.Serializable
     public static final int ISO2022 = 4;
     public static final int MACROMAN = 5;
 
-    /* mode controlling treatment of doctype */
+    /** mode controlling treatment of doctype. */
     public static final int DOCTYPE_OMIT = 0;
     public static final int DOCTYPE_AUTO = 1;
     public static final int DOCTYPE_STRICT = 2;
     public static final int DOCTYPE_LOOSE = 3;
     public static final int DOCTYPE_USER = 4;
 
-    protected int spaces = 2; /* default indentation */
-    protected int wraplen = 68; /* default wrap margin */
+    /** default indentation. */
+    protected int spaces = 2; 
+    /** default wrap margin. */
+    protected int wraplen = 68; 
     protected int CharEncoding = ASCII;
     protected int tabsize = 4;
 
-    protected int docTypeMode = DOCTYPE_AUTO; /* see doctype property */
-    protected String altText = null; /* default text for alt attribute */
-    protected String slidestyle = null; /* style sheet for slides */
-    protected String docTypeStr = null; /* user specified doctype */
-    protected String errfile = null; /* file name to write errors to */
-    protected boolean writeback = false; /* if true then output tidied markup */
+    /** see doctype property. */
+    protected int docTypeMode = DOCTYPE_AUTO; 
+    /** default text for alt attribute. */
+    protected String altText; 
+    /** style sheet for slides. */
+    protected String slidestyle; 
+    /** user specified doctype. */
+    protected String docTypeStr;
+    /** file name to write errors to. */
+    protected String errfile; 
+    /** if true then output tidied markup. */
+    protected boolean writeback; 
 
-    protected boolean OnlyErrors = false; /* if true normal output is suppressed */
-    protected boolean ShowWarnings = true; /* however errors are always shown */
-    protected boolean Quiet = false; /* no 'Parsing X', guessed DTD or summary */
-    protected boolean IndentContent = false; /* indent content of appropriate tags */
-    protected boolean SmartIndent = false; /* does text/block level content effect indentation */
-    protected boolean HideEndTags = false; /* suppress optional end tags */
-    protected boolean XmlTags = false; /* treat input as XML */
-    protected boolean XmlOut = false; /* create output as XML */
-    protected boolean xHTML = false; /* output extensible HTML */
-    protected boolean XmlPi = false; /* add <?xml?> for XML docs */
-    protected boolean RawOut = false; /* avoid mapping values > 127 to entities */
-    protected boolean UpperCaseTags = false; /* output tags in upper not lower case */
-    protected boolean UpperCaseAttrs = false; /* output attributes in upper not lower case */
-    protected boolean MakeClean = false; /* remove presentational clutter */
-    protected boolean LogicalEmphasis = false; /* replace i by em and b by strong */
-    protected boolean DropFontTags = false; /* discard presentation tags */
-    protected boolean DropEmptyParas = true; /* discard empty p elements */
-    protected boolean FixComments = true; /* fix comments with adjacent hyphens */
-    protected boolean BreakBeforeBR = false; /* o/p newline before <br> or not? */
-    protected boolean BurstSlides = false; /* create slides on each h2 element */
-    protected boolean NumEntities = false; /* use numeric entities */
-    protected boolean QuoteMarks = false; /* output " marks as &quot; */
-    protected boolean QuoteNbsp = true; /* output non-breaking space as entity */
-    protected boolean QuoteAmpersand = true; /* output naked ampersand as &amp; */
-    protected boolean WrapAttVals = false; /* wrap within attribute values */
-    protected boolean WrapScriptlets = false; /* wrap within JavaScript string literals */
-    protected boolean WrapSection = true; /* wrap within <![ ... ]> section tags */
-    protected boolean WrapAsp = true; /* wrap within ASP pseudo elements */
-    protected boolean WrapJste = true; /* wrap within JSTE pseudo elements */
-    protected boolean WrapPhp = true; /* wrap within PHP pseudo elements */
-    protected boolean FixBackslash = true; /* fix URLs by replacing \ with / */
-    protected boolean IndentAttributes = false; /* newline+indent before each attribute */
-    protected boolean XmlPIs = false; /* if set to yes PIs must end with ?> */
-    protected boolean XmlSpace = false; /* if set to yes adds xml:space attr as needed */
-    protected boolean EncloseBodyText = false; /* if yes text at body is wrapped in <p> 's */
-    protected boolean EncloseBlockText = false; /* if yes text in blocks is wrapped in <p> 's */
-    protected boolean KeepFileTimes = true; /* if yes last modied time is preserved */
-    protected boolean Word2000 = false; /* draconian cleaning for Word2000 */
-    protected boolean TidyMark = true; /* add meta element indicating tidied doc */
-    protected boolean Emacs = false; /* if true format error output for GNU Emacs */
-    protected boolean LiteralAttribs = false; /* if true attributes may use newlines */
-
-    protected TagTable tt; /* TagTable associated with this Configuration */
+    /** if true normal output is suppressed. */
+    protected boolean OnlyErrors; 
+    /** however errors are always shown. */
+    protected boolean ShowWarnings = true;
+    /** no 'Parsing X', guessed DTD or summary. */
+    protected boolean Quiet; 
+    /** indent content of appropriate tags. */
+    protected boolean IndentContent;
+    /** does text/block level content effect indentation. */
+    protected boolean SmartIndent; 
+    /** suppress optional end tags. */
+    protected boolean HideEndTags;
+    /** treat input as XML. */
+    protected boolean XmlTags;
+    /** create output as XML. */
+    protected boolean XmlOut;
+    /** output extensible HTML. */
+    protected boolean xHTML; 
+    /** add <?xml?> for XML docs. */
+    protected boolean XmlPi; 
+    /** avoid mapping values > 127 to entities. */
+    protected boolean RawOut; 
+    /** output tags in upper not lower case. */
+    protected boolean UpperCaseTags; 
+    /** output attributes in upper not lower case. */
+    protected boolean UpperCaseAttrs;
+    /** remove presentational clutter. */
+    protected boolean MakeClean; 
+    /** replace i by em and b by strong. */
+    protected boolean LogicalEmphasis;
+    /** discard presentation tags. */
+    protected boolean DropFontTags; 
+    /** discard empty p elements. */
+    protected boolean DropEmptyParas = true;
+    /** fix comments with adjacent hyphens. */
+    protected boolean FixComments = true;
+    /** o/p newline before br or not? */
+    protected boolean BreakBeforeBR; 
+    /** create slides on each h2 element. */
+    protected boolean BurstSlides; 
+    /** use numeric entities. */
+    protected boolean NumEntities;
+    /** output " marks as &quot;. */
+    protected boolean QuoteMarks; 
+    /** output non-breaking space as entity. */
+    protected boolean QuoteNbsp = true; 
+    /** output naked ampersand as &amp;. */
+    protected boolean QuoteAmpersand = true;
+    /** wrap within attribute values. */
+    protected boolean WrapAttVals; 
+    /** wrap within JavaScript string literals. */
+    protected boolean WrapScriptlets; 
+    /** wrap within CDATA section tags. */
+    protected boolean WrapSection = true;
+    /** wrap within ASP pseudo elements. */
+    protected boolean WrapAsp = true; 
+    /** wrap within JSTE pseudo elements. */
+    protected boolean WrapJste = true;
+    /** wrap within PHP pseudo elements. */
+    protected boolean WrapPhp = true; 
+    /** fix URLs by replacing \ with /. */
+    protected boolean FixBackslash = true;
+    /** newline+indent before each attribute. */
+    protected boolean IndentAttributes; 
+    /** if set to yes PIs must end with ?>. */
+    protected boolean XmlPIs; 
+    /** if set to yes adds xml:space attr as needed. */
+    protected boolean XmlSpace; 
+    /** if yes text at body is wrapped in p's. */
+    protected boolean EncloseBodyText; 
+    /** if yes text in blocks is wrapped in p's. */
+    protected boolean EncloseBlockText; 
+    /** if yes last modied time is preserved. */
+    protected boolean KeepFileTimes = true; 
+    /** draconian cleaning for Word2000. */
+    protected boolean Word2000; 
+    /** add meta element indicating tidied doc. */
+    protected boolean TidyMark = true;
+    /** if true format error output for GNU Emacs. */
+    protected boolean Emacs; 
+    /** if true attributes may use newlines. */
+    protected boolean LiteralAttribs; 
+    /** TagTable associated with this Configuration. */
+    protected TagTable tt; 
 
     private transient Properties _properties = new Properties();
 
