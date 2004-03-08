ca669c679ae65fb8a391abe5355bdb614c992167
fgiust <fgiust@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2004-03-08 22:41:18 +0000

    from tidy 1.6: fixed CheckAnchor() to call CheckAttributes()

--- src/main/org/w3c/tidy/CheckAttribsImpl.java
+++ src/main/org/w3c/tidy/CheckAttribsImpl.java
@@ -352,6 +352,7 @@ public class CheckAttribsImpl
         public void check(Lexer lexer, Node node)
         {
             node.checkUniqueAttributes(lexer);
+            node.checkAttributes(lexer);
 
             lexer.fixId(node);
         }
