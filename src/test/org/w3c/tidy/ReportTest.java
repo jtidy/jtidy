ca1a7d4d37aa552bebcd51e7c18f6e171a861d2d
fgiust <fgiust@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2004-03-17 20:49:43 +0000

    tests for tidy messages, used during refactoring

---
+++ src/test/org/w3c/tidy/ReportTest.java
@@ -0,0 +1,121 @@
+/**
+ *  Java HTML Tidy - JTidy
+ *  HTML parser and pretty printer
+ *
+ *  Copyright (c) 1998-2000 World Wide Web Consortium (Massachusetts
+ *  Institute of Technology, Institut National de Recherche en
+ *  Informatique et en Automatique, Keio University). All Rights
+ *  Reserved.
+ *
+ *  Contributing Author(s):
+ *
+ *     Dave Raggett <dsr@w3.org>
+ *     Andy Quick <ac.quick@sympatico.ca> (translation to Java)
+ *     Gary L Peskin <garyp@firstech.com> (Java development)
+ *     Sami Lempinen <sami@lempinen.net> (release management)
+ *     Fabrizio Giustina <fgiust at users.sourceforge.net>
+ *
+ *  The contributing author(s) would like to thank all those who
+ *  helped with testing, bug fixes, and patience.  This wouldn't
+ *  have been possible without all of you.
+ *
+ *  COPYRIGHT NOTICE:
+ * 
+ *  This software and documentation is provided "as is," and
+ *  the copyright holders and contributing author(s) make no
+ *  representations or warranties, express or implied, including
+ *  but not limited to, warranties of merchantability or fitness
+ *  for any particular purpose or that the use of the software or
+ *  documentation will not infringe any third party patents,
+ *  copyrights, trademarks or other rights. 
+ *
+ *  The copyright holders and contributing author(s) will not be
+ *  liable for any direct, indirect, special or consequential damages
+ *  arising out of any use of the software or documentation, even if
+ *  advised of the possibility of such damage.
+ *
+ *  Permission is hereby granted to use, copy, modify, and distribute
+ *  this source code, or portions hereof, documentation and executables,
+ *  for any purpose, without fee, subject to the following restrictions:
+ *
+ *  1. The origin of this source code must not be misrepresented.
+ *  2. Altered versions must be plainly marked as such and must
+ *     not be misrepresented as being the original source.
+ *  3. This Copyright notice may not be removed or altered from any
+ *     source or altered source distribution.
+ * 
+ *  The copyright holders and contributing author(s) specifically
+ *  permit, without fee, and encourage the use of this source code
+ *  as a component for supporting the Hypertext Markup Language in
+ *  commercial products. If you use this source code in a product,
+ *  acknowledgment is not required but would be appreciated.
+ *
+ */
+package org.w3c.tidy;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Test for Report messages. <strong>This test case actually requires EN locale to run successfully. </strong>.
+ * @author fgiust
+ * @version $Revision$ ($Author$)
+ */
+public class ReportTest extends TestCase
+{
+
+    /**
+     * report instance.
+     */
+    private Report report;
+
+    /**
+     * lexer instance.
+     */
+    private Lexer lexer;
+
+    /**
+     * instantiates a new test.
+     * @param name test name
+     */
+    public ReportTest(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        this.report = new Report();
+        this.lexer = new Lexer(null, new Configuration(report), this.report);
+        lexer.lines = 12;
+        lexer.columns = 34;
+    }
+
+    /**
+     * test getMessage with the <code>missing_endtag_for</code> key.
+     * @throws Exception any Exception generated during test
+     */
+    public void testGetMessageMissingEndtagFor() throws Exception
+    {
+        String message = this.report.getMessage(lexer, "missing_endtag_for", new Object[]{"test"},
+            TidyMessage.Level.WARNING);
+        assertEquals(message, "line 12 column 34 - Warning: missing </test>");
+    }
+
+    /**
+     * test getMessage with the <code>missing_endtag_before</code> key.
+     * @throws Exception any Exception generated during test
+     */
+    public void testGetMessageMissingEndtagBefore() throws Exception
+    {
+        String message = this.report.getMessage(lexer, "missing_endtag_before", new Object[]{"test", "bee"},
+            TidyMessage.Level.WARNING);
+        assertEquals(message, "line 12 column 34 - Warning: missing </test> before bee");
+    }
+
+
+}
