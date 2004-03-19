5c7b75fa729c5efcf0ac0732858b33d28f62ec61
fgiust <fgiust@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2004-03-19 13:07:22 +0000

    cleaned up, removed unused contructor, javadocs added

--- src/main/org/w3c/tidy/Attribute.java
+++ src/main/org/w3c/tidy/Attribute.java
@@ -1,116 +1,127 @@
 /**
-*  Java HTML Tidy - JTidy
-*  HTML parser and pretty printer
-*
-*  Copyright (c) 1998-2000 World Wide Web Consortium (Massachusetts
-*  Institute of Technology, Institut National de Recherche en
-*  Informatique et en Automatique, Keio University). All Rights
-*  Reserved.
-*
-*  Contributing Author(s):
-*
-*     Dave Raggett <dsr@w3.org>
-*     Andy Quick <ac.quick@sympatico.ca> (translation to Java)
-*     Gary L Peskin <garyp@firstech.com> (Java development)
-*     Sami Lempinen <sami@lempinen.net> (release management)
-*     Fabrizio Giustina <fgiust at users.sourceforge.net>
-*
-*  The contributing author(s) would like to thank all those who
-*  helped with testing, bug fixes, and patience.  This wouldn't
-*  have been possible without all of you.
-*
-*  COPYRIGHT NOTICE:
-* 
-*  This software and documentation is provided "as is," and
-*  the copyright holders and contributing author(s) make no
-*  representations or warranties, express or implied, including
-*  but not limited to, warranties of merchantability or fitness
-*  for any particular purpose or that the use of the software or
-*  documentation will not infringe any third party patents,
-*  copyrights, trademarks or other rights. 
-*
-*  The copyright holders and contributing author(s) will not be
-*  liable for any direct, indirect, special or consequential damages
-*  arising out of any use of the software or documentation, even if
-*  advised of the possibility of such damage.
-*
-*  Permission is hereby granted to use, copy, modify, and distribute
-*  this source code, or portions hereof, documentation and executables,
-*  for any purpose, without fee, subject to the following restrictions:
-*
-*  1. The origin of this source code must not be misrepresented.
-*  2. Altered versions must be plainly marked as such and must
-*     not be misrepresented as being the original source.
-*  3. This Copyright notice may not be removed or altered from any
-*     source or altered source distribution.
-* 
-*  The copyright holders and contributing author(s) specifically
-*  permit, without fee, and encourage the use of this source code
-*  as a component for supporting the Hypertext Markup Language in
-*  commercial products. If you use this source code in a product,
-*  acknowledgment is not required but would be appreciated.
-*
-*/
+ *  Java HTML Tidy - JTidy
+ *  HTML parser and pretty printer
+ *
+ *  Copyright (c) 1998-2000 World Wide Web Consortium (Massachusetts
+ *  Institute of Technology, Institut National de Recherche en
+ *  Informatique et en Automatique, Keio University). All Rights
+ *  Reserved.
+ *
+ *  Contributing Author(s):
+ *
+ *     Dave Raggett <dsr@w3.org>
+ *     Andy Quick <ac.quick@sympatico.ca> (translation to Java)
+ *     Gary L Peskin <garyp@firstech.com> (Java development)
+ *     Sami Lempinen <sami@lempinen.net> (release management)
+ *     Fabrizio Giustina <fgiust at users.sourceforge.net>
+ *
+ *  The contributing author(s) would like to thank all those who
+ *  helped with testing, bug fixes, and patience.  This wouldn't
+ *  have been possible without all of you.
+ *
+ *  COPYRIGHT NOTICE:
+ * 
+ *  This software and documentation is provided "as is," and
+ *  the copyright holders and contributing author(s) make no
+ *  representations or warranties, express or implied, including
+ *  but not limited to, warranties of merchantability or fitness
+ *  for any particular purpose or that the use of the software or
+ *  documentation will not infringe any third party patents,
+ *  copyrights, trademarks or other rights. 
+ *
+ *  The copyright holders and contributing author(s) will not be
+ *  liable for any direct, indirect, special or consequential damages
+ *  arising out of any use of the software or documentation, even if
+ *  advised of the possibility of such damage.
+ *
+ *  Permission is hereby granted to use, copy, modify, and distribute
+ *  this source code, or portions hereof, documentation and executables,
+ *  for any purpose, without fee, subject to the following restrictions:
+ *
+ *  1. The origin of this source code must not be misrepresented.
+ *  2. Altered versions must be plainly marked as such and must
+ *     not be misrepresented as being the original source.
+ *  3. This Copyright notice may not be removed or altered from any
+ *     source or altered source distribution.
+ * 
+ *  The copyright holders and contributing author(s) specifically
+ *  permit, without fee, and encourage the use of this source code
+ *  as a component for supporting the Hypertext Markup Language in
+ *  commercial products. If you use this source code in a product,
+ *  acknowledgment is not required but would be appreciated.
+ *
+ */
 package org.w3c.tidy;
 
 /**
  * HTML attribute.
- * @author Dave Raggett <a href="mailto:dsr@w3.org">dsr@w3.org</a>
- * @author Andy Quick <a href="mailto:ac.quick@sympatico.ca">ac.quick@sympatico.ca</a> (translation to Java)
+ * @author Dave Raggett <a href="mailto:dsr@w3.org">dsr@w3.org </a>
+ * @author Andy Quick <a href="mailto:ac.quick@sympatico.ca">ac.quick@sympatico.ca </a> (translation to Java)
+ * @author Fabrizio Giustina
  * @version $Revision $ ($Author $)
  */
 public class Attribute
 {
+
+    /**
+     * attribute name.
+     */
     private String name;
+
+    /**
+     * don't wrap attribute.
+     */
     private boolean nowrap;
+
+    /**
+     * unmodifiable attribute?
+     */
     private boolean literal;
+
+    /**
+     * html versions for this attribute.
+     */
     private short versions;
+
+    /**
+     * checker for the attribute.
+     */
     private AttrCheck attrchk;
 
-    public Attribute(String name, boolean nowrap, short versions, AttrCheck attrchk)
-    {
-        this.name = name;
-        this.nowrap = nowrap;
-        this.literal = false;
-        this.versions = versions;
-        this.attrchk = attrchk;
-    }
-
-    public Attribute(String name, short versions, AttrCheck attrchk)
-    {
-        this.name = name;
-        this.nowrap = false;
-        this.literal = false;
-        this.versions = versions;
-        this.attrchk = attrchk;
-    }
-
     /**
-     * @param literal The literal to set.
+     * Instantiates a new Attribute.
+     * @param attributeName attribute name
+     * @param htmlVersions versions in which this attribute is supported
+     * @param check AttrCheck instance
      */
-    public void setLiteral(boolean literal)
+    public Attribute(String attributeName, short htmlVersions, AttrCheck check)
     {
-        this.literal = literal;
+        this.name = attributeName;
+        this.versions = htmlVersions;
+        this.attrchk = check;
     }
 
     /**
-     * @param name The name to set.
+     * Is this a literal (unmodifiable?) attribute?
+     * @param isLiteral boolean <code>true</code> for a literal attribute
      */
-    public void setName(String name)
+    public void setLiteral(boolean isLiteral)
     {
-        this.name = name;
+        this.literal = isLiteral;
     }
 
     /**
-     * @param nowrap The nowrap to set.
+     * Don't wrap this attribute?
+     * @param isNowrap boolean <code>true</code>= don't wrap
      */
-    public void setNowrap(boolean nowrap)
+    public void setNowrap(boolean isNowrap)
     {
-        this.nowrap = nowrap;
+        this.nowrap = isNowrap;
     }
 
     /**
-     * @return Returns the attrchk.
+     * Returns the checker for this attribute.
+     * @return instance of AttrCheck.
      */
     public AttrCheck getAttrchk()
     {
@@ -118,7 +129,8 @@ public class Attribute
     }
 
     /**
-     * @return Returns the literal.
+     * Is this a literal (unmodifiable?) attribute?
+     * @return <code>true</code> for a literal attribute
      */
     public boolean isLiteral()
     {
@@ -126,7 +138,8 @@ public class Attribute
     }
 
     /**
-     * @return Returns the name.
+     * Returns the attribute name.
+     * @return attribute name.
      */
     public String getName()
     {
@@ -134,7 +147,8 @@ public class Attribute
     }
 
     /**
-     * @return Returns the nowrap.
+     * Don't wrap this attribute?
+     * @return <code>true</code>= don't wrap
      */
     public boolean isNowrap()
     {
@@ -142,7 +156,9 @@ public class Attribute
     }
 
     /**
-     * @return Returns the versions.
+     * Returns the html versions in which this attribute is supported.
+     * @return html versions for this attribute.
+     * @see Dict
      */
     public short getVersions()
     {
