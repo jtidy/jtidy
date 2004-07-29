6f4582b5e44eb0c64daf699112d1946c0d974d05
fgiust <fgiust@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2004-07-29 20:36:44 +0000

    added VERS_XHTML11

--- src/main/org/w3c/tidy/Dict.java
+++ src/main/org/w3c/tidy/Dict.java
@@ -66,27 +66,49 @@ public class Dict
     /* content model shortcut encoding */
 
     public static final int CM_UNKNOWN = 0;
+
     public static final int CM_EMPTY = (1 << 0);
+
     public static final int CM_HTML = (1 << 1);
+
     public static final int CM_HEAD = (1 << 2);
+
     public static final int CM_BLOCK = (1 << 3);
+
     public static final int CM_INLINE = (1 << 4);
+
     public static final int CM_LIST = (1 << 5);
+
     public static final int CM_DEFLIST = (1 << 6);
+
     public static final int CM_TABLE = (1 << 7);
+
     public static final int CM_ROWGRP = (1 << 8);
+
     public static final int CM_ROW = (1 << 9);
+
     public static final int CM_FIELD = (1 << 10);
+
     public static final int CM_OBJECT = (1 << 11);
+
     public static final int CM_PARAM = (1 << 12);
+
     public static final int CM_FRAMES = (1 << 13);
+
     public static final int CM_HEADING = (1 << 14);
+
     public static final int CM_OPT = (1 << 15);
+
     public static final int CM_IMG = (1 << 16);
+
     public static final int CM_MIXED = (1 << 17);
+
     public static final int CM_NO_INDENT = (1 << 18);
+
     public static final int CM_OBSOLETE = (1 << 19);
+
     public static final int CM_NEW = (1 << 20);
+
     public static final int CM_OMITST = (1 << 21);
 
     /*
@@ -99,31 +121,49 @@ public class Dict
     public static final short VERS_UNKNOWN = 0;
 
     public static final short VERS_HTML20 = 1;
+
     public static final short VERS_HTML32 = 2;
+
     public static final short VERS_HTML40_STRICT = 4;
+
     public static final short VERS_HTML40_LOOSE = 8;
+
     public static final short VERS_FRAMESET = 16;
+
     public static final short VERS_XML = 32;
 
     public static final short VERS_NETSCAPE = 64;
+
     public static final short VERS_MICROSOFT = 128;
+
     public static final short VERS_SUN = 256;
 
     public static final short VERS_MALFORMED = 512;
 
+    public static final short VERS_XHTML11 = 1024;
+
     public static final short VERS_ALL = (VERS_HTML20 | VERS_HTML32 | VERS_HTML40_STRICT | VERS_HTML40_LOOSE | VERS_FRAMESET);
+
     public static final short VERS_HTML40 = (VERS_HTML40_STRICT | VERS_HTML40_LOOSE | VERS_FRAMESET);
+
     public static final short VERS_LOOSE = (VERS_HTML32 | VERS_HTML40_LOOSE | VERS_FRAMESET);
+
     public static final short VERS_IFRAME = (VERS_HTML40_LOOSE | VERS_FRAMESET);
+
     public static final short VERS_FROM32 = (VERS_HTML40_STRICT | VERS_LOOSE);
+
     public static final short VERS_PROPRIETARY = (VERS_NETSCAPE | VERS_MICROSOFT | VERS_SUN);
 
     public static final short VERS_EVERYTHING = (VERS_ALL | VERS_PROPRIETARY);
 
     public String name;
+
     public short versions;
+
     public int model;
+
     public Parser parser;
+
     public CheckAttribs chkattrs;
 
     public Dict(String name, short versions, int model, Parser parser, CheckAttribs chkattrs)
