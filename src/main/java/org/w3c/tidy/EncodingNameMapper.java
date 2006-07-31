ad20ef9921f07921d95a4efc39738f69ec547ca6
fgiust <fgiust@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2006-07-31 19:55:29 +0000

    [1284197] EncodingNameMapper.toJava("WINDOWS-31J") returns null

--- src/main/java/org/w3c/tidy/EncodingNameMapper.java
+++ src/main/java/org/w3c/tidy/EncodingNameMapper.java
@@ -20,14 +20,14 @@
  *  have been possible without all of you.
  *
  *  COPYRIGHT NOTICE:
- * 
+ *
  *  This software and documentation is provided "as is," and
  *  the copyright holders and contributing author(s) make no
  *  representations or warranties, express or implied, including
  *  but not limited to, warranties of merchantability or fitness
  *  for any particular purpose or that the use of the software or
  *  documentation will not infringe any third party patents,
- *  copyrights, trademarks or other rights. 
+ *  copyrights, trademarks or other rights.
  *
  *  The copyright holders and contributing author(s) will not be
  *  liable for any direct, indirect, special or consequential damages
@@ -43,7 +43,7 @@
  *     not be misrepresented as being the original source.
  *  3. This Copyright notice may not be removed or altered from any
  *     source or altered source distribution.
- * 
+ *
  *  The copyright holders and contributing author(s) specifically
  *  permit, without fee, and encourage the use of this source code
  *  as a component for supporting the Hypertext Markup Language in
@@ -212,7 +212,7 @@ public abstract class EncodingNameMapper
         encodingNameMap.put("MS936", new String[]{"GBK", "GBK"});
 
         encodingNameMap.put("MS932", new String[]{"WINDOWS-31J", "MS932"});
-        encodingNameMap.put("WINDOWS-31J", new String[]{"WINDOWS-31J", "MS932"});
+        encodingNameMap.put("31J", new String[]{"WINDOWS-31J", "MS932"});
         encodingNameMap.put("CSWINDOWS31J", new String[]{"WINDOWS-31J", "MS932"});
         encodingNameMap.put("TIS-620", new String[]{"TIS-620", "TIS620"});
         encodingNameMap.put("TIS620", new String[]{"TIS-620", "TIS620"});
