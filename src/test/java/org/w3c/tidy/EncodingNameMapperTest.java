e83fd7f678b3b9d626b4d3058f7d67ff0573fc37
aditsu <aditsu@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2010-07-31 16:27:44 +0000

    Applied patch from bug 3036734 - EncodingNameMapperTest.java.patch

--- src/test/java/org/w3c/tidy/EncodingNameMapperTest.java
+++ src/test/java/org/w3c/tidy/EncodingNameMapperTest.java
@@ -99,7 +99,7 @@ public class EncodingNameMapperTest extends TestCase
         assertEquals("MacRoman", EncodingNameMapper.toJava("Macintosh Roman"));
         assertEquals("Cp1252", EncodingNameMapper.toJava("WiN1252"));
         assertEquals("SJIS", EncodingNameMapper.toJava("SHIFTJIS"));
-        assertEquals("31J", EncodingNameMapper.toJava("WINDOWS-31J"));
+        assertEquals("MS932", EncodingNameMapper.toJava("WINDOWS-31J"));
         assertEquals(null, EncodingNameMapper.toJava("IBM-"));
     }
 
